ad12b046060bc510be86472518cc722a9cea4eb6
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2020-09-21 16:02:55 +0200

    Update server

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
-io.on('connection', () => console.log('User connected'))
+io.on('connection', (socket) => {
+  console.log('User connected')
+
+  socket.on('chat message', (message) => {
+    io.emit('chat message', message)
+  })
+})
 
 server.listen(3000)
