3959292ff5bd9b91149fab6196db7ae1e6bf1082
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2020-10-01 10:43:31 +0200

    Update server

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 
 const timeFormat = new Intl.DateTimeFormat('sv', {
-  timeStyle: 'medium',
+  timeStyle: 'short',
   timeZone: 'Europe/Stockholm',
 })
 
@@ -11,20 +11,52 @@ app.use(cors())
 
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
+let connectedUsers = []
 
 if (process.env.NODE_ENV === 'production') {
   io.origins(['https://socket-client.sonnerberg.me:443'])
 }
 
 io.on('connection', (socket) => {
-  console.log('User connected')
-  io.emit('chat message', `${timeFormat.format(Date.now())}: User connected`)
+  const time = `${timeFormat.format(Date.now())}`
+  const message = 'User connected to the chat, awaiting nickname selection...'
+  io.emit('chat message', {
+    time,
+    message,
+  })
 
-  socket.on('chat message', (nickname, message) => {
-    io.emit(
-      'chat message',
-      `${timeFormat.format(Date.now())} ${nickname}: ${message}`,
-    )
+  socket.on('chat message', ({ name, message }) => {
+    const time = `${timeFormat.format(Date.now())}`
+    io.emit('chat message', {
+      time,
+      name,
+      message,
+    })
+  })
+
+  socket.on('join room', ({ name }) => {
+    const time = `${timeFormat.format(Date.now())}`
+    const message = 'joined the chat.'
+    connectedUsers = [...connectedUsers, { name, id: socket.id }]
+    io.emit('join room', {
+      time,
+      name,
+      message,
+      connectedUsers,
+    })
+  })
+
+  socket.on('disconnect', () => {
+    const time = `${timeFormat.format(Date.now())}`
+    const leaver = connectedUsers.filter((user) => user.id === socket.id)
+    connectedUsers = connectedUsers.filter((user) => user.id !== socket.id)
+    io.emit('leave room', {
+      connectedUsers,
+    })
+    io.emit('chat message', {
+      time,
+      message: `${leaver.length ? leaver[0]['name'] : 'User'} left the chat`,
+    })
   })
 })
 
